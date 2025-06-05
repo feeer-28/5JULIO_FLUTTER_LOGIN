e134028ff99140012cc5c709f9e24f3516eeafba
feeer-28 <darcyfernandagonzalez@gmail.com>
2025-06-05 18:26:42 -0500

    flutterLogin

---
+++ lib/views/loginScreen.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_junio5/views/mainPrincipal.dart';
+
+class LoginScreen extends StatefulWidget {
+    const LoginScreen({super.key});
+
+    @override
+    State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+    final Color fondo = const Color(0xFF60B5FF);
+    final Color encabezado = Color(0xFF3A7DFF);
+    final Color campos = const Color(0xFFFFECDE);
+    final Color botones = const Color(0xFFFF9149);
+    final Color texto = const Color(0xFF000000);
+    
+
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            backgroundColor: fondo,
+            body: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: ListView(
+                shrinkWrap: true,
+                children: [
+                Icon(Icons.person_pin, size: 100, color: botones),
+                SizedBox(height: 20),
+                Text("Bienvenido", 
+                    style: TextStyle(
+                        fontSize: 30, 
+                        color: campos, 
+                        fontWeight: FontWeight.bold
+                    ),
+                    textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 20),
+                Text("inicia sesion para continuar", 
+                    style: TextStyle(
+                        fontSize: 20, 
+                        color: texto, 
+                        fontWeight: FontWeight.w400
+                    ),
+                    textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 20),
+                TextField(
+                    style: TextStyle(color: texto),
+                    decoration: InputDecoration(
+                        filled: true,
+                        fillColor: campos,
+                        labelText: "Correo Electronico",
+                        labelStyle: TextStyle(color: texto),
+                        prefixIcon: Icon(Icons.email, color: texto),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide.none,
+                        ),
+                        
+                    )
+                    ,
+                    ),
+                SizedBox(height: 20),
+                TextField(
+                    obscureText: true,
+                    style: TextStyle(color: texto),
+                    decoration: InputDecoration(
+                        filled: true,
+                        fillColor: campos,
+                        labelText: "Contraseña",
+                        labelStyle: TextStyle(color: texto),
+                        prefixIcon: Icon(Icons.lock, color: texto),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide.none,
+                        ),
+                    ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                    onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(
+                            builder: (context) => MenuPrincipal()
+                        ));},
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: botones,
+                        minimumSize: Size(double.infinity, 50),
+                    ),
+                    child: Text(
+                        "Iniciar Sesion",
+                        style: TextStyle(
+                        color: texto,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        ),
+                    ),
+                ),
+                SizedBox(height: 20),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                            Text(
+                                "¿olvidaste tu contraseña?",
+                                style: TextStyle(
+                                    color: texto,
+                                    fontSize: 16,
+                                ),
+                            ),
+                            /*GestureDetector(
+                                onTap: () {
+                                },
+                                child: Text(
+                                    " Recuperar",
+                                    style: TextStyle(
+                                        color: botones,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                    ),
+                                ),
+                            ),*/
+                            TextButton(
+                                onPressed: () {
+                                    
+                                },
+                                child: Text(
+                                    " Recuperar",
+                                    style: TextStyle(
+                                        color: botones,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                    ),
+                                ),
+                            ),
+                                SizedBox(width: 20,),
+                            ],
+                        ),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                                Text(
+                                    "¿No tienes una cuenta?",
+                                    style: TextStyle(
+                                        color: texto,
+                                        fontSize: 16,
+                                    ),
+                                ),
+                                TextButton(
+                                    onPressed: () {
+                                    },
+                                    child: Text(
+                                        " Registrate",
+                                        style: TextStyle(
+                                            color: botones,
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold,
+                                        ),
+                                    ),
+                                ),
+                            ],
+                        ),
+                        // Aquí puedes agregar más widgets dentro del Padding
+                    ],
+                ),
+            ),
+        );
+    }
+    }
---
+++ lib/views/mainPrincipal.dart
@@ -0,0 +1,205 @@
+import 'package:flutter/material.dart';
+
+class MenuPrincipal extends StatefulWidget {
+  const MenuPrincipal({super.key});
+
+  @override
+  State<MenuPrincipal> createState() => _MenuPrincipalState();
+}
+
+class _MenuPrincipalState extends State<MenuPrincipal> {
+  final Color fondo = const Color(0xFF60B5FF);
+  final Color primario = Color(0xFF3A7DFF);
+  final Color secundario = const Color(0xFFFFECDE);
+  final Color detalle = const Color(0xFFFF9149);
+  final Color texto = const Color(0xFF000000);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: fondo,
+      appBar: AppBar(
+        title: Text("alquiler de vehiculos"),
+        backgroundColor: primario,
+        foregroundColor: texto,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search, color: texto),
+                hintText: "Buscar vehiculo",
+                // ignore: deprecated_member_use
+                hintStyle: TextStyle(color: texto.withOpacity(0.5)),
+                filled: true,
+                fillColor: secundario,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: listaAutos.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    leading: Image.network(
+                      listaAutos[index]["imageUrl"],
+                      width: 100,
+                      height: 100,
+                      fit: BoxFit.cover,
+                    ),
+                    title: Text(
+                      "${listaAutos[index]["marca"]} ${listaAutos[index]["modelo"]}",
+                      style: TextStyle(color: texto, fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Año: ${listaAutos[index]["anio"]}"),
+                        Text("Precio por día: \$${listaAutos[index]["precio"]}"),
+                        Text(
+                          "Disponibilidad: ${listaAutos[index]["disponibilidad"] ? "Disponible" : "No disponible"}",
+                          style: TextStyle(
+                            color: listaAutos[index]["disponibilidad"]
+                                ? Colors.green
+                                : Colors.red,
+                          ),
+                        ),
+                      ],
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.arrow_forward, color: detalle),
+                      onPressed: () {
+                        // Acción al presionar el botón
+                      },
+                    ),
+                  );
+                  },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/// Lista de autos
+final List<Map<String, dynamic>> listaAutos = [
+  {
+    "imageUrl": "https://picsum.photos/300/200?1",
+    "marca": "Toyota",
+    "modelo": "Corolla",
+    "anio": 2020,
+    "disponibilidad": true,
+    "precio": 150.0,
+  },
+  {
+    "imageUrl": "https://picsum.photos/300/200?2",
+    "marca": "Honda",
+    "modelo": "Civic",
+    "anio": 2019,
+    "disponibilidad": false,
+    "precio": 140.0,
+  },
+  {
+    "imageUrl": "https://picsum.photos/300/200?3",
+    "marca": "Ford",
+    "modelo": "Focus",
+    "anio": 2018,
+    "disponibilidad": true,
+    "precio": 130.0,
+  },
+  {
+    "imageUrl": "https://picsum.photos/300/200?4",
+    "marca": "Chevrolet",
+    "modelo": "Cruze",
+    "anio": 2021,
+    "disponibilidad": true,
+    "precio": 160.0,
+  },
+  {
+    "imageUrl": "https://picsum.photos/300/200?5",
+    "marca": "Nissan",
+    "modelo": "Sentra",
+    "anio": 2017,
+    "disponibilidad": false,
+    "precio": 120.0,
+  },
+  {
+    "imageUrl": "https://picsum.photos/300/200?6",
+    "marca": "Mazda",
+    "modelo": "3",
+    "anio": 2022,
+    "disponibilidad": true,
+    "precio": 170.0,
+  },
+  {
+    "imageUrl": "https://picsum.photos/300/200?7",
+    "marca": "Volkswagen",
+    "modelo": "Jetta",
+    "anio": 2019,
+    "disponibilidad": true,
+    "precio": 145.0,
+  },
+  {
+    "imageUrl": "https://picsum.photos/300/200?8",
+    "marca": "Hyundai",
+    "modelo": "Elantra",
+    "anio": 2020,
+    "disponibilidad": false,
+    "precio": 135.0,
+  },
+  {
+    "imageUrl": "https://picsum.photos/300/200?9",
+    "marca": "Kia",
+    "modelo": "Rio",
+    "anio": 2018,
+    "disponibilidad": true,
+    "precio": 125.0,
+  },
+  {
+    "imageUrl": "https://picsum.photos/300/200?10",
+    "marca": "Renault",
+    "modelo": "Logan",
+    "anio": 2021,
+    "disponibilidad": true,
+    "precio": 155.0,
+  },
+  {
+    "imageUrl": "https://picsum.photos/300/200?11",
+    "marca": "Peugeot",
+    "modelo": "208",
+    "anio": 2017,
+    "disponibilidad": false,
+    "precio": 115.0,
+  },
+  {
+    "imageUrl": "https://picsum.photos/300/200?12",
+    "marca": "Fiat",
+    "modelo": "Argo",
+    "anio": 2022,
+    "disponibilidad": true,
+    "precio": 165.0,
+  },
+  {
+    "imageUrl": "https://picsum.photos/300/200?13",
+    "marca": "BMW",
+    "modelo": "Serie 3",
+    "anio": 2020,
+    "disponibilidad": true,
+    "precio": 250.0,
+  },
+  {
+    "imageUrl": "https://picsum.photos/300/200?15",
+    "marca": "Audi",
+    "modelo": "A4",
+    "anio": 2021,
+    "disponibilidad": true,
+    "precio": 260.0,
+  },
+];
